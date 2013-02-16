382f1c36c9576e6cfd70311c950db6bedef9f244
Alex Byrnes <alex@nuams.co>
2013-02-15 19:25:21 -0600

    minor

--- wsgi/config.py
+++ wsgi/config.py
@@ -9,7 +9,7 @@ DB_PASSWORD = os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD')
 DEBUG = True 
 SERVER_NAME = '127.0.0.1'
 SERVER_PORT = 80 
-DB_CONNECTION = "host='{0}' port='{1}' dbname='{2} user='{3}' password='{4}'".format(DB_HOST, DB_PORT, APP_NAME, DB_USER, DB_PASSWORD)
+DB_CONNECTION = "host='{0}' port='{1}' dbname='{2}' user='{3}' password='{4}'".format(DB_HOST, DB_PORT, APP_NAME, DB_USER, DB_PASSWORD)
 
 METADATA_DB = 'metadb'
 METADATA_COLLECTION = 'metadata'
